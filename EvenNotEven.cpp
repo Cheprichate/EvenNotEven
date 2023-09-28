6e80c5b7470d23822439def7f939536f86d8834d
Cheprichate <petrovich15071998@mail.ru>
2023-09-28 21:00:48 +0400

    Добавьте файлы проекта.

---
+++ EvenNotEven.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int a;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите число : ";
+    std::cin >> a;
+    if (a%2==0){
+        std::cout << a << " Четное число.";
+    }else{
+        std::cout << a << " Не четное число. ";
+    }
+}
